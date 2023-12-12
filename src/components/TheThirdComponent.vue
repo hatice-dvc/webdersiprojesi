291a4282ca9b70ce1d6db729598bb66228399238
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-12-12 23:01:07 +0300

    CarouselComponent added

--- src/components/TheThirdComponent.vue
+++ src/components/TheThirdComponent.vue
@@ -16,6 +16,27 @@
         :name="1"
         img-src="https://n11scdn.akamaized.net/a1/1180_440/23/12/08/67/78/58/61/33/73/94/78/77/94460984954828340413.jpg"
       >
+        <div>
+          <q-carousel-control
+            position="top-right"
+            :offset="[100, 100]"
+            class="text-white rounded-borders"
+            style="
+              background: #f6f5f7;
+              height: 180px;
+              width: 350px;
+              display: flex;
+              align-items: flex-end;
+            "
+          >
+            <div style="padding: 20px 20px 0 8px; color: #5a3d8c; font-weight: bold;">
+              <p style="margin-top: 50px; font-size: 13px;">Sabah 09.00'a kadar geçerli!</p>
+              <p style="font-size: 28px;">Gecenin sürprizini kaçırma!</p>
+            </div>
+
+            <q-btn color="purple" text-color="white" label="Ürüne Git " />/>
+          </q-carousel-control>
+        </div>
       </q-carousel-slide>
       <q-carousel-slide
         :name="2"
@@ -44,7 +65,7 @@
         </div>
       </q-carousel-slide>
       <q-carousel-slide/>
-      <q-carousel-slide :name="3" img-src="https://n11scdn.akamaized.net/a1/1180_440/23/12/09/64/64/58/98/75/21/37/55/68/64757561397295401386.jpg" />
+      <q-carousel-slide :name="3" img-src="https://n11scdn.akamaized.net/a1/1180_440/23/12/09/64/64/58/98/75/21/37/55/68/64757561397295401386.jpg"/>
       <q-carousel-slide :name="4" img-src="https://n11scdn.akamaized.net/a1/1180_440/23/12/08/58/03/49/80/37/91/66/45/89/23603055386349056343.jpg" />
       <q-carousel-slide :name="5" img-src="https://n11scdn.akamaized.net/a1/1180_440/23/12/08/79/92/99/07/10/82/28/28/89/51324589523414895162.jpg" />
       <q-carousel-slide :name="6" img-src="https://n11scdn.akamaized.net/a1/1180_440/23/12/09/51/22/16/31/83/04/61/22/76/77092101554917033911.jpg" />
@@ -68,4 +89,5 @@ export default {
     }
   }
 }
+
 </script>
